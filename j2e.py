8d294548c4a537e0fed9c3fecd0db7bab041af5a
C4RnaG3 <35183248+C4RnaG3@users.noreply.github.com>
2019-03-21 08:50:10 +0000

    Update j2e.py

--- j2e.py
+++ j2e.py
@@ -2,12 +2,15 @@
 
 # -*- coding: utf-8 -*-
 
-#-- Used for Jiami to English translation for later use
+#-- to do list:
+#--     Clean up the output
+
+# Tested against a php script that simply echoed a chinese string, and it was successfully decoded
 
 #-- Basic libraries
-import sys, argparse, codecs
+import sys, argparse
 
-#-- Imports the Translator module from the googletrans library (needs to be installed)
+#-- Imports the Translator module from the googletrans library (needs to be installed -- pip install googletrans)
 from googletrans import Translator
 
 #----------------------
@@ -24,6 +27,9 @@ jfile=f.read()
 translator = Translator(service_urls=['translate.google.com'])
 
 #-- https://github.com/ssut/py-googletrans/blob/master/googletrans/constants.py
+
 translations = translator.translate([jfile], dest='en')
 for translation in translations:
+#-- translates the unicode to its English equivelant
     print(translation.origin, ' -> ', translation.text)
+    
